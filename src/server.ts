f56caaa6ea25486043b4ec88e50fc35ac50aaf71
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-27 13:46:21 -0300

    ✨ NEW FEATURE: create specifications route

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
+
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 
 app.listen(3333, () => {
   console.log("🚀 Server is running!");
-  console.log("💻 Access: http://localhost:3333")
+  console.log("💻 Access: http://localhost:3333");
 });
