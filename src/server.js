ba261f7f0c665e1d7d91cfd0275bd49638b97584
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-03-27 07:19:06 -0300

    ✨ NEW FEATURE: create category route

--- src/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = express_1["default"]();
-app.listen(3333, function () { console.log("🚀 Server is running on 3333!"); });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
+app.use(express.json())
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Fala aí galera" });
-})
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("🚀 Server is running!");
