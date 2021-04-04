d208f8b402417d73a99774086bcacf207493c057
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-04-04 18:29:50 -0300

    ✨ NEW FEATURE: error handling

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password incorrect", 401);
     }
 
     const passwordMatches = compare(password, user.password);
 
     if (!passwordMatches) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password incorrect", 401);
     }
 
     const token = sign({}, "0ef64cdda33d9ea5da6cddb56ee42197", {
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -20,7 +21,8 @@ class CreateSpecificationUseCase {
     );
 
     if (specificationAlreadyExists) {
-      throw new Error("Specification already exists");
+      console.log("Especificação existe.");
+      throw new AppError("Specification already exists");
     }
 
     await this.specificationsRepository.create({
