e112953c7b9a3b889dfefddf80287b4a24426fe9
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-04-04 11:28:29 -0300

    ✨ NEW FEATURE: authenticate user route

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,57 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const passwordMatches = compare(password, user.password);
+
+    if (!passwordMatches) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "0ef64cdda33d9ea5da6cddb56ee42197", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -17,12 +17,12 @@ class CreateUserUseCase {
     password,
     driver_licence,
   }: ICreateUserDTO): Promise<void> {
-    const userAlreadyExists = this.usersRepository.findByEmail(email):
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
       throw new Error("User already exists");
     }
-    
+
     const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
