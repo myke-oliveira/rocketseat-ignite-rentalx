8d856fb05bad42b4243d681ef024b5716578c714
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-04-11 18:13:10 -0300

    ✔️ PASS: create category use case

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,15 +1,47 @@
-describe("Create category", () => {
-  it("Espero que 2 + 2 seja 4", () => {
-    const soma = 2 + 2;
-    const resultado = 4;
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepository } from "../../repositories/in-memory/CategoriesRepository";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-    expect(soma).toBe(resultado);
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoryRepository: CategoriesRepository;
+
+describe("Create category", () => {
+  beforeAll(() => {
+    categoryRepository = new CategoriesRepository();
+    createCategoryUseCase = new CreateCategoryUseCase(categoryRepository);
+  });
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description test",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoryRepository.findByName(category.name);
+
+    expect(categoryCreated).toHaveProperty("id");
   });
 
-  it("Espero que 2 + 2 não seja 5", () => {
-    const soma = 2 + 2;
-    const resultado = 5;
+  it("should not be able to create a new category with a name that already exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test 2",
+        description: "Category description test 2",
+      };
 
-    expect(soma).not.toBe(resultado);
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
