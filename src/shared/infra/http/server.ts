60cb02bd8683bd269de593fe5100d1e228ffc555
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-05-02 18:47:32 -0300

    🗄️ DB: seed admin user

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import createConnection from "@shared/infra/typeorm";
+
 import swaggerFile from "../../../swagger.json";
 import { AppError } from "../../errors/AppError";
 import { router } from "./routes";
-
-import "@shared/infra/typeorm";
 
 import "@shared/container";
 
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+createConnection();
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
